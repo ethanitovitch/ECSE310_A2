f88ac5a90695de055193ce9ba7353818a804cf5c
Louca Dufault <louca.dufault@gmail.com>
2021-04-03 20:50:59 -0400

    improve

--- fft.py
+++ fft.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 import argparse
 import time
@@ -12,7 +10,7 @@ import os
 
 DEFAULT_MODE = 1
 DEFAULT_IMAGE_FILE_NAME = 'moonlanding.png'
-LOW_FREQUENCY_PROPORTION = 0.1 # the lowest 10% of frequencies are considered "low"
+LOW_FREQUENCY_PROPORTION = 0.1  # the lowest 10% of frequencies are considered "low"
 
 
 class IllegalArgumentError(ValueError):
@@ -29,6 +27,8 @@ def naiveFourierTransform(array, inverse=False):
 
 
 def evens(array): return array[::2]
+
+
 def odds(array): return array[1::2]
 
 
@@ -44,10 +44,11 @@ def fastFourierTransform(array, inverse=False):
 
     complex_part = 2j if inverse else -2j
     divide = N if inverse else 1
-    
+
     factor = lambda m: np.exp(complex_part * np.pi * m / N)
 
-    return [divide * X_even[m] + divide * factor(m) * X_odds[m] for m in range(N//2)] + [divide * X_even[m] - divide * factor(m) * X_odds[m] for m in range(N//2)] 
+    return [divide * X_even[m] + divide * factor(m) * X_odds[m] for m in range(N // 2)] + [
+        divide * X_even[m] - divide * factor(m) * X_odds[m] for m in range(N // 2)]
 
 
 def naiveFourierTransformMatrix(matrix, inverse=False):
@@ -73,21 +74,22 @@ def fastFourierTransformMatrix(matrix, inverse=False):
         for col in range(len(row_result[0]))
     ]).T
 
+
 def resizeToPowerOf2(image_file_name):
     img = cv2.imread(image_file_name, cv2.IMREAD_GRAYSCALE)
     x = img.shape[1]
     x_log2 = math.log2(x)
-    x_floor = 2**math.floor(x_log2)
-    x_ceil = 2**math.ceil(x_log2)
-    x = x_ceil if abs(x - x_floor) > abs(x-x_ceil) else x_floor
+    x_floor = 2 ** math.floor(x_log2)
+    x_ceil = 2 ** math.ceil(x_log2)
+    x = x_ceil if abs(x - x_floor) > abs(x - x_ceil) else x_floor
 
     y = img.shape[0]
     y_log2 = math.log2(y)
-    y_floor = 2**math.floor(y_log2)
-    y_ceil = 2**math.ceil(y_log2)
+    y_floor = 2 ** math.floor(y_log2)
+    y_ceil = 2 ** math.ceil(y_log2)
     y = y_ceil if abs(y - y_floor) > abs(y - y_ceil) else y_floor
 
-    return cv2.resize(img, (x, y), interpolation = cv2.INTER_AREA)
+    return cv2.resize(img, (x, y), interpolation=cv2.INTER_AREA)
 
 
 def modeOne(image_file_name):
@@ -103,7 +105,8 @@ def modeOne(image_file_name):
 
     plt.show()
 
-def maskFrequencies(X, above=True, mask_thresh=1-LOW_FREQUENCY_PROPORTION): # mask higher: X, True, 0.1
+
+def maskFrequencies(X, above=True, mask_thresh=1 - LOW_FREQUENCY_PROPORTION):  # mask higher: X, True, 0.1
     rows, cols = X.shape
     if above:
         X[math.floor(rows * (1 - mask_thresh)):math.ceil(rows * mask_thresh)] = 0
@@ -114,18 +117,6 @@ def maskFrequencies(X, above=True, mask_thresh=1-LOW_FREQUENCY_PROPORTION): # ma
         X[:, 0:math.floor(cols * (1 - mask_thresh))] = 0
         X[:, math.ceil(cols * mask_thresh):] = 0
     return X
-
-# a = np.random.randint(low=1, high=10, size=(32,32))
-# print(a)
-# print(a.shape)
-# for row in a:
-#     print(row)
-# masked = maskFrequencies(np.array(a).copy(), above=True)
-# print("\n\n")
-# for row in masked:
-#     print(row)
-#
-# print(masked.ravel()[masked.ravel() != 0])
 
 
 def modeTwo(image_file_name):
@@ -145,30 +136,34 @@ def modeTwo(image_file_name):
     plt.show()
 
 
-def saveComplexMatrixAsMinimizedTxt(fileName, array, delimiter=' '):
+def saveMatrixAsMinimizedTxt(file_name, array, delimiter=' '):
     """
-    Save complex matrix to txt file with the same formatting as np.savetxt, but with complex exponentials equal to 0 (i.e. real and imaginary part are 0) written as a compact '0' to minimize file size.
+    Save complex matrix to txt file with the elements in the same formatting as np.savetxt, but with complex numbers with value equal to 0 (i.e. real and imaginary part are 0) written as a compact '0' to minimize file size.
     
     e.g. '(0.000000000000000000e+00+0.000000000000000000e+00j)' -> '0'
     """
+
     def formatComplex(x, fmt='%.18e'): return "({}{}{}j)".format(fmt % x.real, '+' if x.imag > 0 else '', fmt % x.imag)
-    with open(fileName, 'w') as f:
+
+    with open(file_name, 'w') as f:
         for row in array:
             line = delimiter.join("0" if value == 0 else formatComplex(value) for value in row)
             f.write(line + '\n')
 
 
-class CompressionStrategy(): # enum
-    LargestAll = 0 # threshold the coefficients’ magnitude and take only the largest percentile of them
-    AllLowsAndLargestHighs = 1 # keep all the coefficients of very low frequencies as well as a fraction of the largest coefficients from higher frequencies to also filter the image at the same time
+class CompressionStrategy():  # enum
+    LargestAll = 0  # threshold the coefficients’ magnitude and take only the largest percentile of them
+    AllLowsAndLargestHighs = 1  # keep all the coefficients of very low frequencies as well as a fraction of the largest coefficients from higher frequencies to also filter the image at the same time
 
-def modeThree(image_file_name, compression_strategy=CompressionStrategy.LargestAll): #, compression_strategy):
+
+def modeThree(image_file_name, compression_strategy=CompressionStrategy.LargestAll):  # , compression_strategy):
     if compression_strategy == 0:
         print('Compression Strategy: LargestAll')
     elif compression_strategy == 1:
         print('Compression Strategy: AllLowsAndLargestHighs')
     else:
-        raise IllegalArgumentError("Compression strategy must be one of 'LargestAll' (0) or 'AllLowsAndLargestHighs' (1)")
+        raise IllegalArgumentError(
+            "Compression strategy must be one of 'LargestAll' (0) or 'AllLowsAndLargestHighs' (1)")
 
     max_compression = 99
     img = resizeToPowerOf2(image_file_name)
@@ -190,12 +185,13 @@ def modeThree(image_file_name, compression_strategy=CompressionStrategy.LargestA
     print(coefficient_count)
 
     fig, ax = plt.subplots(2, 3)
-    for i, compression_level in enumerate(np.linspace(0, max_compression, num=6, dtype=np.int_)):  
+    for i, compression_level in enumerate(np.linspace(0, max_compression, num=6, dtype=np.int_)):
         threshold = sorted_coefficients[int(compression_level * coefficient_count / 100)]
 
-        mask = np.abs(X) >= threshold # matrix with the same shape as X with 1s where the magnitude of the coefficients are >= threshold and 0s elsewhere 
-        X_largest_coefficients = mask * X # mask the coefficients with magnitudes < threshold
-        
+        mask = np.abs(
+            X) >= threshold  # matrix with the same shape as X with 1s where the magnitude of the coefficients are >= threshold and 0s elsewhere
+        X_largest_coefficients = mask * X  # mask the coefficients with magnitudes < threshold
+
         if compression_strategy == 0:
             compressed_X = X_largest_coefficients
         elif compression_strategy == 1:
@@ -203,13 +199,13 @@ def modeThree(image_file_name, compression_strategy=CompressionStrategy.LargestA
             compressed_X = np.where(X_low_frequencies != 0, X_low_frequencies, X_largest_coefficients)
 
         file_name = str(compression_level) + '%.txt'
-        saveComplexMatrixAsMinimizedTxt(file_name, compressed_X)
+        saveMatrixAsMinimizedTxt(file_name, compressed_X)
 
         compressed_image = np.abs(np.fft.ifft2(compressed_X))
 
         title = 'Original' if i == 0 else str(compression_level) + '%'
-        ax[i//3, i%3].imshow(compressed_image, cmap='gray', vmin=0, vmax=255)
-        ax[i//3, i%3].title.set_text(title)
+        ax[i // 3, i % 3].imshow(compressed_image, cmap='gray', vmin=0, vmax=255)
+        ax[i // 3, i % 3].title.set_text(title)
 
         size = os.stat(file_name)
         print(
@@ -224,12 +220,11 @@ def modeThree(image_file_name, compression_strategy=CompressionStrategy.LargestA
 
 def modeFour():
     EXPERIMENTS = 10
-    MAX_MATRIX_SIZE_POWER = 6 # 2^7 takes ~10s
-    CONFIDENCE_INTERVAL = 0.97 # idk
+    MAX_MATRIX_SIZE_POWER = 6  # 2^7 takes ~10s
     data = {}
 
     for i in range(MAX_MATRIX_SIZE_POWER):
-        size = 2**i
+        size = 2 ** i
         data[size] = {"naive": [], "fast": []}
 
         for j in range(EXPERIMENTS):
@@ -239,7 +234,7 @@ def modeFour():
             naiveFourierTransformMatrix(random_matrix)
             end = time.perf_counter()
             data[size]["naive"].append(end - start)
-            
+
             start = time.perf_counter()
             fastFourierTransformMatrix(random_matrix)
             end = time.perf_counter()
@@ -250,10 +245,10 @@ def modeFour():
     naive_means = [np.mean(data[size]["naive"]) for size in sorted_sizes]
     fast_means = [np.mean(data[size]["fast"]) for size in sorted_sizes]
 
-    naive_stds = [np.std(data[size]["naive"]) for size in sorted_sizes]
-    fast_stds = [np.std(data[size]["fast"]) for size in sorted_sizes]
+    naive_errors = [2 * np.std(data[size]["naive"]) for size in sorted_sizes]
+    fast_errors = [2 * np.std(data[size]["fast"]) for size in sorted_sizes]
 
-    print("Experiments: {}\nConfidence Interval: {}%".format(EXPERIMENTS, CONFIDENCE_INTERVAL*100))
+    print("Experiments per problem size: {}\nConfidence Interval: {}%".format(EXPERIMENTS, 97))
 
     # plot
     labels = sorted_sizes
@@ -263,8 +258,8 @@ def modeFour():
 
     width = 0.35  # the width of the bars
     capsize = 5
-    naive_bars = ax.bar(x - width/2, naive_means, yerr=naive_stds, width=width, label="Naïve", capsize=capsize)
-    fast_bars = ax.bar(x + width/2, fast_means, yerr=fast_stds, width=width, label="Fast", capsize=capsize)
+    naive_bars = ax.bar(x - width / 2, naive_means, yerr=naive_errors, width=width, label="Naïve", capsize=capsize)
+    fast_bars = ax.bar(x + width / 2, fast_means, yerr=fast_errors, width=width, label="Fast", capsize=capsize)
 
     ax.set_title("Runtimes by problem size for naïve and fast Fourier transform")
     ax.set_xlabel("Problem size")
@@ -285,43 +280,46 @@ def printError(message, prefix="ERROR"):
 
 
 def getParams():
-    parser = argparse.ArgumentParser(exit_on_error=False) # if there is an error raised by this line, please consult the README.md
+    parser = argparse.ArgumentParser(
+        exit_on_error=False)  # if there is an error raised by this line, please consult the README.md
     parser.add_argument("-m", "--mode", type=int, help="mode (optional)", default=DEFAULT_MODE)
-    parser.add_argument("-i", "--image", type=str, help="image (optional) filename of the image we wish to take the DFT of", default=DEFAULT_IMAGE_FILE_NAME)
-    
-    try:
-        namespace, rest = parser.parse_known_args() # https://docs.python.org/3/library/argparse.html#argparse.ArgumentParser.parse_known_args
+    parser.add_argument("-i", "--image", type=str,
+                        help="image (optional) filename of the image we wish to take the DFT of",
+                        default=DEFAULT_IMAGE_FILE_NAME)
 
-        fatals = 0
+    try:
+        namespace, rest = parser.parse_known_args()  # https://docs.python.org/3/library/argparse.html#argparse.ArgumentParser.parse_known_args
+
         for arg in rest:
             raise IllegalArgumentError("Incorrect input syntax: '{}' argument is not recognized".format(arg))
 
         params = vars(namespace)
 
-        if (params["mode"] < 1 or params["mode"] > 4):
+        if params["mode"] < 1 or params["mode"] > 4:
             raise IllegalArgumentError("Incorrect input syntax: mode must be between 1 and 4")
-        
+
         params["image"] = params["image"].strip()
-        
+
         return params
 
     except argparse.ArgumentError as e:
         raise IllegalArgumentError("Incorrect input syntax: " + str(e))
-        # assume fatal
+
 
 def main():
     mode = 0
     image_file_name = ""
+
     try:
         params = getParams()
         mode = params["mode"]
         image_file_name = params["image"]
     except IllegalArgumentError as e:
         printError(str(e))
-    
+
     print("Mode " + str(mode) + "\n")
 
-    if mode ==  1:
+    if mode == 1:
         modeOne(image_file_name)
     elif mode == 2:
         modeTwo(image_file_name)
